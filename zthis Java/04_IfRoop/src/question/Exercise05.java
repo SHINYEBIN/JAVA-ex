c7138c23b40dabaa386e93ef0ff1ab3829a13ffd
EMILY <92853470+SHINYEBIN@users.noreply.github.com>
2022-06-01 21:37:33 +0900

    4장 ex-5 해결 못함/ ex-6은 해결

--- zthis Java/04_IfRoop/src/question/Exercise05.java
+++ zthis Java/04_IfRoop/src/question/Exercise05.java
@@ -1,9 +1,18 @@
 package question;
-
+// 중첩 for문 사용하여 4x+5y=60 해 구해서 (x,y)형태로 출력. 단 x와 y는 10 이하 자연수.
 public class Exercise05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x=0;
+	
+		int y=0;
+		int result=60;
+		System.out.println("("+x+"), ("+y+")");
+		for(x=1;x<10;x++) {
+			for(y=1;y<10;y++) {
+				 result=4*x+5*y;
+			}
+		}
 
 	}
 
---
+++ zthis Java/04_IfRoop/src/question/Exercise06.java
@@ -0,0 +1,17 @@
+package question;
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		String result = "";
+		String star="*";
+		for(int i=1;i<=5;i++) {
+			result+=star;
+			System.out.println(result);
+		}
+		
+		
+
+	}
+
+}
