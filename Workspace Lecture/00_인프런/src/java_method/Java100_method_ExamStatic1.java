b6ff9d5fd09155fc506baee51472a5f0f3529ce7
EMILY <92853470+SHINYEBIN@users.noreply.github.com>
2022-08-12 23:56:28 +0900

    메서드 개념

---
+++ Workspace Lecture/00_인프런/src/java_method/Java100_method_ExamStatic1.java
@@ -0,0 +1,14 @@
+package java_method;
+// 이 문제는 자바의 메서드 구현시 기본적인 주의점에 대해서 아는지를 묻는 문제
+// static으로 선언을 해주면 클래스명.메서드명();으로 사용 가능!
+// static은 static을 호출한다.(부른다.)
+public class Java100_method_ExamStatic1 {
+	
+	public static void helloWorld() {
+		System.out.println("Hello, World^~^");
+	}
+	public static void main(String[]args) {
+		helloWorld();
+	}
+}
+
